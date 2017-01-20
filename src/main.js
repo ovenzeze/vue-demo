fe1b7b41dd3f45def5e9c78a6ed00732038ab0fb
ovenzeze <ovenzeze@qq.com>
2017-01-20 10:45:46 +0800

    初始化

---
+++ src/main.js
@@ -0,0 +1,44 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import store from './store'
+
+// 组件
+import App from './App'
+import Home from './components/Home'
+import TimeEntries from './components/TimeEntries.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const routes = [{
+  path : '/',
+  component : Home
+},{
+  path : '/home',
+  component : Home
+},{
+  path : '/time-entries',
+  component : TimeEntries,
+  children : [{
+    path : 'log-time',
+    // 懒加载
+    component : resolve => require(['./components/LogTime.vue'],resolve),
+  }]
+}];
+
+const router = new VueRouter({
+  routes
+});
+
+/* eslint-disable no-new */
+// 实例化我们的Vue
+var app = new Vue({
+  el: '#app',
+  router,
+  store,
+  ...App,
+});
